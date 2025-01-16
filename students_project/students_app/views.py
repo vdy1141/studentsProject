89f3e78e6e6f802c9162a4016d7af7916bf4dade
vdy1141 <vdy1141@gmail.com>
2025-01-16 18:51:31 +0530

    Update views.py

--- students_project/students_app/views.py
+++ students_project/students_app/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def home_view(request):
     return HttpResponse("<h1>This is Home Page</h1>")
+
+
+def about_us(request):
+    return HttpResponse("<h1>This is About us Page</h1>")
